b9ad6e1dc5686ebf94a231b0926799b87a3946b1
Владислава Лифар <vladislava.lufar@MacBook-Air.local>
2026-05-13 17:21:19 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   base: '/landmate-2-/'
 })
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> b67341e0c599b0f1c7aa01df80108c39b0b904b4
